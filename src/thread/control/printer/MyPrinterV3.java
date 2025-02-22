9415e0a1cfe76d36ee7d2edb3654d3cf2bf2bc26
Goathoon <ownest2@gmail.com>
2025-02-22 19:32:35 +0900

    [2장]: interrupt 메서드로 깔끔하게 처리하기

--- src/thread/control/printer/MyPrinterV3.java
+++ src/thread/control/printer/MyPrinterV3.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static thread.util.MyLogger.log;
-import static thread.util.ThreadUtils.sleep;
 
-public class MyPrinterV2 {
+public class MyPrinterV3 {
     public static void main(String[] args) {
         Printer printer = new Printer();
         Thread printerThread = new Thread(printer, "printer");
@@ -18,7 +17,6 @@ public class MyPrinterV2 {
             log("프린터할 문서를 입력하세요. 종료 (q):");
             String input = userInput.nextLine();
             if (input.equals("q")) {
-                printer.work = false;
                 printerThread.interrupt();
                 break;
             }
@@ -28,12 +26,11 @@ public class MyPrinterV2 {
     }
 
     static class Printer implements Runnable{
-        volatile boolean work = true;
-        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+        Queue<String> jobQueue = new   ConcurrentLinkedQueue<>();
 
         @Override
         public void run() {
-            while(work) {
+            while(!Thread.interrupted()) {
                 try {
                     String job = jobQueue.poll();
                     log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
