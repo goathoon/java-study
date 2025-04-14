172372382c27bcb276ce9e89e55084283c657d88
Goathoon <ownest2@gmail.com>
2025-04-14 23:00:14 +0900

    [7장]: 원자적 연산을 위한 여러가지 예시 및 AtomicInteger 성능 테스트 -> CAS의 발걸음

---
+++ src/thread/cas/increment/SynchronizedInteger.java
@@ -0,0 +1,16 @@
+package thread.cas.increment;
+
+public class SynchronizedInteger implements IncrementInteger{
+
+    private int value;
+
+    @Override
+    synchronized public void increment() {
+        value++;
+    }
+
+    @Override
+    synchronized public int get() {
+        return value;
+    }
+}
