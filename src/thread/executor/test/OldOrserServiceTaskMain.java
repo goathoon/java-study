8efcbdfb2b911bd37865e078cb5fec42e047beb6
Goathoon <ownest2@gmail.com>
2025-04-16 22:18:57 +0900

    [9장]: 문제 풀이

---
+++ src/thread/executor/test/OldOrserServiceTaskMain.java
@@ -0,0 +1,12 @@
+package thread.executor.test;
+
+import java.util.concurrent.ExecutionException;
+
+public class OldOrserServiceTaskMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        String orderNo = "Order#1234";
+        MyOldOrderService orderService = new MyOldOrderService();
+        orderService.order(orderNo);
+    }
+}
