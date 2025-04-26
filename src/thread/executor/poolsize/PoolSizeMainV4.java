e4f36c959c161fd00ce8fb55d66d82a9df5a8848
Goathoon <ownest2@gmail.com>
2025-04-26 16:57:18 +0900

    [10장]: Executor 예외 정책들에 대해 알아보기

--- src/thread/executor/poolsize/PoolSizeMainV4.java
+++ src/thread/executor/poolsize/PoolSizeMainV4.java
@@ -12,7 +12,6 @@ public class PoolSizeMainV4 {
 //    static final int TASK_SIZE = 1100; // 1. 일반
 //    static final int TASK_SIZE = 1200; // 2. 긴급
     static final int TASK_SIZE = 1201; // 3. 거절
->>>>>
     public static void main(String[] args) {
         ExecutorService es = new ThreadPoolExecutor(100, 200,
                 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1000));
