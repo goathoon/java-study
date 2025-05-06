a6ac1c0b34a7d3285e4febdf416e8677177bc8bb
Goathoon <ownest2@gmail.com>
2025-05-06 21:52:06 +0900

    [2장]: Buffer의 사이즈를 무한대로 늘리면 성능향상이 지속적으로 되지 않는다. Buffer크기를 1 -> 2로 늘렸을때는 성능이 두배로 향상되는걸 볼 수 있지만, 8KB이상일 경우 디스크나 파일시스템에서 데이터를 읽고 쓰는 단위가 기본으로 4KB 혹은 8KB이기 때문이다.

---
+++ src/java2/io/buffered/CreateFileV2.java
@@ -0,0 +1,39 @@
+package java2.io.buffered;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static java2.io.buffered.BufferedConst.*;
+
+public class CreateFileV2 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        long startTime = System.currentTimeMillis();
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bufferIndex = 0;
+
+        for (int i = 0; i < FILE_SIZE; i++) {
+            buffer[bufferIndex++ ] = 1;
+
+            if (bufferIndex == BUFFER_SIZE) {
+                fos.write(buffer);
+                bufferIndex = 0;
+            }
+        }
+
+        // 끝 부분에 오면 버퍼가 가득차지 않고, 남아있을 수 있다. 버퍼에 남은 부분 쓰기
+        if(bufferIndex > 0) {
+            fos.write(buffer,0, bufferIndex);
+        }
+
+        fos.close();
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+        /*
+        이렇게 하면 굉장히 오랜시간이 걸린다.. 최적화가 필요함
+         */
+    }
+}
