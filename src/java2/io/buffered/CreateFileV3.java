2db1bbd09787bf2b04a35ebcec29146e88f5ce54
Goathoon <ownest2@gmail.com>
2025-05-08 00:33:38 +0900

    [2장]: BufferedOutputStream으로 buffer 구현 없이 성능 최적화하기. 이를 보조스트림이라고 한다 (독단적으로 사용할 수 없음)

---
+++ src/java2/io/buffered/CreateFileV3.java
@@ -0,0 +1,28 @@
+package java2.io.buffered;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static java2.io.buffered.BufferedConst.*;
+
+public class CreateFileV3 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        BufferedOutputStream bos = new BufferedOutputStream(fos, BUFFER_SIZE);
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < FILE_SIZE; i++) {
+            bos.write(1);
+        }
+        bos.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+        /*
+        이렇게 하면 굉장히 오랜시간이 걸린다.. 최적화가 필요함
+         */
+    }
+}
