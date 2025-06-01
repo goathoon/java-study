25e321dcbda2a1f782dfd3019759af741cb4cdfc
Goathoon <ownest2@gmail.com>
2025-06-01 19:05:01 +0900

    [6장]: 앞선 문제들은 해결됐지만, 코드 퀄리티가 낮다. 1. 변수 스코프 문제 2. catch 이후 finally로 자원정리 3. 개발자의 실수로 인한 close()호출 생략 가능성 4. close() 호출 순서 실수로 인한 문제발생 가능성

---
+++ src/java2/network/tcp/autocloseable/ResourceCloseMainV3.java
@@ -0,0 +1,48 @@
+package java2.network.tcp.autocloseable;
+
+public class ResourceCloseMainV3 {
+    public static void main(String[] args) {
+        try {
+            logic();
+        } catch (CallException e) {
+            System.out.println("Call Exception 예외 처리");
+            throw new RuntimeException(e);
+        } catch (CloseException e) {
+            System.out.println("Close Exception 예외 처리");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void logic() throws CallException, CloseException {
+        ResourceV1 resource1 = null;
+        ResourceV1 resource2 = null;
+        try {
+            resource1 = new ResourceV1("resource1");
+            resource2 = new ResourceV1("resource2");
+
+            resource1.call();
+            resource2.callEx();
+        } catch (CallException e) {
+            System.out.println("ex: " + e);
+            throw e;
+        } finally {
+            System.out.println("자원 정리"); // 호출 안됨
+            // 자원 열었던 순서와 반대로 닫아야한다! (물론 여기에서는 참조가 없긴 하지만)
+            if(resource2 != null) {
+                try {
+                    resource2.closeEx();
+                } catch (CloseException e) {
+                    // close()에서 발생한 예외는 버린다. 필요하면 로깅 정
+                    System.out.println("close ex: " + e);
+                }
+            }
+            if(resource1 != null) {
+                try {
+                    resource1.closeEx();
+                } catch (CloseException e) {
+                    System.out.println("close ex: " + e);
+                }
+            }
+        }
+    }
+}
