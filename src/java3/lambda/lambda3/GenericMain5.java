de828f8ec5c9f9fcfb6f879dbe4994a46ded09b6
Goathoon <ownest2@gmail.com>
2025-08-24 16:45:58 +0900

    [3장]: Generic을 통해 동일한 함수인터페이스를 재사용할 수 있다

---
+++ src/java3/lambda/lambda3/GenericMain5.java
@@ -0,0 +1,19 @@
+package java3.lambda.lambda3;
+
+public class GenericMain5 {
+
+    public static void main(String[] args) {
+        GenericFunction<String, String> upperCase = s -> s.toUpperCase();
+        String result1 = upperCase.apply("hello");
+        System.out.println("result1 = " + result1);
+
+        GenericFunction<Integer, Integer> square = n ->  n * n;
+        Integer result2 = square.apply(5);
+        System.out.println("result2 = " + result2);
+    }
+
+    @FunctionalInterface
+    interface GenericFunction<T, R> {
+         R apply(T s);
+    }
+}
